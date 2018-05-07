4384a50456da20ec0be45897a208766063062d95
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-05-07 18:39:15 +0900

    devise view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:new_event]
+
+  def index
+    @events = Event.all
+  end
+
   def new_event
+    @event = Event.find_by(id: params[:id])
   end
 end
