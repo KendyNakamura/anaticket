f92cc93421467df7dcd2754839491f841c41a7ba
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-05-24 14:41:44 +0900

    timedate to timestamp

--- db/migrate/20180516092650_change_column_to_events.rb
+++ db/migrate/20180516092650_change_column_to_events.rb
@@ -1,5 +1,6 @@
 class ChangeColumnToEvents < ActiveRecord::Migration[5.2]
   def change
-    change_column :events, :start_time, :datetime
+    change_column :events, :start_time, 'timestamp with time zone'
+    # change_column :events, :start_time, :datetime
   end
 end
