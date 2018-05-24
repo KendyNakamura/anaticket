655387661c6b67f6bc5f8cf9edb5272d53d8e948
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-05-24 15:04:12 +0900

    datetime time

--- db/migrate/20180516092650_change_column_to_events.rb
+++ db/migrate/20180516092650_change_column_to_events.rb
@@ -1,6 +1,6 @@
 class ChangeColumnToEvents < ActiveRecord::Migration[5.2]
   def change
-    remove_column :events, :start_time, :datetime
+    remove_column :events, :start_time, :time
     add_column :events, :start_time, :datetime
   end
 end
