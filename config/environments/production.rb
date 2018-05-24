515175798513f4faaf28754b975db698bd7c78a7
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-05-24 22:17:40 +0900

    mail.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,29 +92,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # config.action_mailer.default_url_options = { host: 'git.heroku.com' }
-  # mailer
   # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: 'anaticket.heroku.com' }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_deliveries = true
   # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: 'heroku.com',
+  #   address: 'smtp.sendgrid.net',
   #   port: 587,
   #   authentication: :plain,
-  #   user_name: Rails.application.credentials.SMTP_EMAIL,
-  #   password: Rails.application.credentials.SMTP_PASSWORD
+  #   enable_starttls_auto: true
   # }
-  # Setup the mailer config
-  # Use SendGrid - Add-ons - Heroku
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'anaticket.heroku.com' }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
 end
