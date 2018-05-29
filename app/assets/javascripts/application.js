b0f9238a3760c4313bf2bfd910af06ae4f239367
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-05-30 06:27:41 +0900

    bootstrap適用

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,6 @@
 //= require_tree .
 
 // カレンダーフォームの作成
-
 $(function () {
   var dt = new Date()
   var maxdt = dt.setDate(dt.getDate() + 59);
@@ -29,14 +28,22 @@ $(function () {
       format: 'YYYY/MM/DD HH:mm',
       stepping: 15,
       minDate: Date(),
-      maxDate: maxdt
+      maxDate: maxdt,
+      widgetPositioning: {
+          horizontal: 'right',
+          vertical: 'bottom'
+      }
     });
     var finish_time = $('#start_time').data("DateTimePicker")
     $('#finish_time').datetimepicker({
         useCurrent: false, //Important! See issue #1075
         format: 'YYYY/MM/DD HH:mm',
         stepping: 15,
-        maxDate: maxdt
+        maxDate: maxdt,
+        widgetPositioning: {
+            horizontal: 'right',
+            vertical: 'bottom'
+        }
     });
     $("#start_time").on("dp.change", function (e) {
         $('#finish_time').data("DateTimePicker").minDate(e.date);
